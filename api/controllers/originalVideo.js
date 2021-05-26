793ce38cd0b56b13367f36e2fa52bf9a6e9a5b78
gilad-tamar <giladtamarr@gmail.com>
2021-05-26 15:38:15 +0300

    originals videos get all revert

--- api/controllers/originalVideo.js
+++ api/controllers/originalVideo.js
@@ -24,47 +24,7 @@ module.exports = {
             ));
         });
     
-        
-        var videosPath = 'C:\\collage\\final\\leagueTok-server\\videos';
-
-        //Create new folder for the original video data
-        try {
-                 fs.mkdirSync(`${videosPath}\\4`);
-                console.log(`Directory created successfully! - ${videosPath}\\4`);
-
-        } catch (error) {
-            console.log('Directory Create Failed!');
-            res.status(500);
-              return res.send(`failed create directory ${videosPath}\\4 \n error ${error}`);
-        }
-        //Run OpenPose
-        //var url = 'C:\\collage\\final\\leagueTok-server\\videos\\3\\source3.mp4'
-        var url = 'https://firebasestorage.googleapis.com/v0/b/leaguetok.appspot.com/o/videos%2FOriginals%2F3rls61Ny7xVNTZlg2IDZ.mp4?alt=media&token=44b05aff-bc81-4656-9fbd-b20eb1887544'
-        exec(`bin\\OpenPoseDemo.exe --video "${url}" --write_json "${videosPath}\\4\\openpose" --net_resolution 320x320 --part_candidates`,
-        {
-            cwd: 'C:\\collage\\final\\openpose\\openposeGPU'
-        }, (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                res.status(500);
-                return res.send(`OpenPose Filed error: ${error}`);
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-
-            //Create new folder in videos directory for imitations
-            try {
-                fs.mkdirSync(`${videosPath}\\4\\Imitations`);
-                console.log(`Directory created successfully! - ${videosPath}\\4\\Imitations`);
-            } catch (error) {
-                console.log(`Directory Create Failed! - ${videosPath}\\4\\Imitations \n error- ${error}` );
-            }
-            res.status(200).send({ id: 4 , videos});
-            
-        })
+        res.status(200).send(videos);
     },
 
     create: async (req, res) => {
