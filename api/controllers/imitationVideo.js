696b28bc8499acd89540479d8321c504e9b19d91
Nir Naiman <206769390@STUD.colman.ac.il>
2021-05-30 16:03:57 +0300

    one record to one person

--- api/controllers/imitationVideo.js
+++ api/controllers/imitationVideo.js
@@ -9,15 +9,55 @@ module.exports = {
     createVideo: async(req, res) => {
     const { sourceId, link, uid } = req.body;
     var timestamp = admin.firestore.FieldValue.serverTimestamp();
-    var imitVideo = new ImitationVideo(null, link, uid, sourceId, 0, timestamp, timestamp, false);
+
+//  Check if uid and sourceId allready exits in the db. 
+//  If so, update the record instead of creating a new one.
+    var imitVideo = null;
+    var isNew = false;
 
     try{
-      imitVideo.id = (await database.collection(IMITATION_VIDEOS_COLL).add(imitVideo.getObject())).id
+      var snapshot = await database.collection(IMITATION_VIDEOS_COLL).where("sourceId", "==", sourceId).where("uid", "==", uid)
+      .get();    
     } catch(err){
       console.log('Failed')
       res.status(500)
       res.send('failed')
     }
+    
+    snapshot.forEach((doc) => {
+      // .then((querySnapshot) => {
+        // querySnapshot.forEach((doc) => {
+        data = doc.data();
+//      ImitVideo exists
+
+        imitVideo = new ImitationVideo(doc.id, data.url, data.uid, data.sourceId, data.score, data.uploadDate, data.lastUpdated,
+                                       data.isDeleted);
+    });
+    // });
+
+//  ImitVideo does not exists
+    if (imitVideo == null) {
+      isNew = true;
+      imitVideo = new ImitationVideo(null, link, uid, sourceId, 0, timestamp, timestamp, false);
+
+      try{
+        imitVideo.id = (await database.collection(IMITATION_VIDEOS_COLL).add(imitVideo.getObject())).id
+      } catch(err){
+        console.log('Failed')
+        res.status(500)
+        res.send('failed')
+      }
+    } 
+
+    // var imitVideo = new ImitationVideo(null, link, uid, sourceId, 0, timestamp, timestamp, false);
+
+    // try{
+    //   imitVideo.id = (await database.collection(IMITATION_VIDEOS_COLL).add(imitVideo.getObject())).id
+    // } catch(err){
+    //   console.log('Failed')
+    //   res.status(500)
+    //   res.send('failed')
+    // }
 
     let options = { 
       args: ["./videos/1/openpose", "./videos/1/Imitations/1/openpose"] //An argument which can be accessed in the script using sys.argv[1]
@@ -30,17 +70,36 @@ module.exports = {
           res.send('Failed on python');
           return;
         }
-        imitVideo.score = Number(result[0])
-
-        try{
+//      If the record is new
+        if (isNew) {
+          imitVideo.score = Math.round(Number(result[0]));
           await database.collection(IMITATION_VIDEOS_COLL).doc(imitVideo.id).update(imitVideo.getObject());
-        } catch(err){
-          console.log('Failed')
-          res.status(500)
-          res.send('failed')
-        }
 
-        res.send({"result": result[0]})
+        } else {
+//        The record is not new
+//        Does this dance better than what exists already? If yes update, else don't. 
+          if (imitVideo.score < Number(result[0])) {
+
+            imitVideo.score = Number(result[0])
+  
+           // Round the score
+  
+            try{
+              await database.collection(IMITATION_VIDEOS_COLL).doc(imitVideo.id).update({
+                score: Math.round(imitVideo.score), 
+                url: link, 
+                uploadDate: timestamp,
+                lastUpdated: timestamp 
+              });
+            } catch(err){
+              console.log('Failed')
+              res.status(500)
+              res.send('failed')
+            }
+          }  
+        }
+        res.send({"result": (Math.round(Number(result[0]))).toString()})
+        // res.send({"result": result[0]})
       });
     } catch(err){
       console.log(err)
